d7e5bde8b95aab11d6161e0ba5a2bd9943db4659
PhanThaoTien <23130328@st.hcmuaf.edu.vn>
2026-05-04 22:20:28 +0700

    Kết thúc ván, hiển thị kết quả
    Added methods to check winning lines and collect directions.

--- CNPM_Nhom9/src/model/Board.java
+++ CNPM_Nhom9/src/model/Board.java
@@ -25,6 +25,55 @@ public class Board {
 		int player = isXTurn ? 2 : 1;
 		return getWinLine(row, col, player) != null;
 	}
+	public int[][] getWinLine(int row, int col) {
+		int player = isXTurn ? 2 : 1;
+		return findWinLineOnBoard(player);
+	}
+	
+	private int[][] findWinLineOnBoard(int player) {
+		int[][] directions = { { 0, 1 }, { 1, 0 }, { 1, 1 }, { 1, -1 } };
+
+		for (int r = 0; r < SIZE; r++) {
+			for (int c = 0; c < SIZE; c++) {
+				if (matrix[r][c] != player)
+					continue;
+
+				for (int[] dir : directions) {
+					java.util.List<int[]> line = new java.util.ArrayList<>();
+					line.add(new int[] { r, c });
+
+					int nr = r + dir[0], nc = c + dir[1];
+					while (nr >= 0 && nr < SIZE && nc >= 0 && nc < SIZE && matrix[nr][nc] == player) {
+						line.add(new int[] { nr, nc });
+						nr += dir[0];
+						nc += dir[1];
+					}
+
+					if (line.size() >= WIN_CONDITION) {
+						return line.toArray(new int[0][]);
+					}
+				}
+			}
+		}
+		return null;
+	}
+	public int[][] getWinLine(int row, int col, int player) {
+		int[][] directions = { { 0, 1 }, { 1, 0 }, { 1, 1 }, { 1, -1 } };
+		for (int[] dir : directions) {
+			java.util.List<int[]> forward = collectDirection(row, col, dir[0], dir[1], player);
+			java.util.List<int[]> backward = collectDirection(row, col, -dir[0], -dir[1], player);
+
+			if (forward.size() + backward.size() + 1 >= WIN_CONDITION) {
+				java.util.List<int[]> line = new java.util.ArrayList<>();
+				java.util.Collections.reverse(backward);
+				line.addAll(backward);
+				line.add(new int[] { row, col });
+				line.addAll(forward);
+				return line.toArray(new int[0][]);
+			}
+		}
+		return null;
+	}
 	
 	public boolean isBoardFull() {
 		for (int i = 0; i < SIZE; i++)
